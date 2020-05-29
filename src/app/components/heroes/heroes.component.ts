df227e0c283b7c9244c1914afacbf01370a93e3f
Daniel Lopez <ext_delopezp@falabella.cl>
2020-05-28 22:16:37 -0400

    componente individual

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,13 +11,18 @@ export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
 
   constructor(
-    private heroesService: HeroesService
+    private heroesService: HeroesService,
+    private router: Router
   ) {
   }
 
   ngOnInit(): void {
     this.heroes = this.heroesService.getHeroes();
-    console.log(this.heroes);
+    // console.log(this.heroes);
+  }
+
+  verHeroe(idx: number) {
+    this.router.navigate(['/heroe', idx]);
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -64,6 +64,10 @@ export class HeroesService {
   getHeroes(): Heroe[] {
     return this.heroes;
   }
+
+  getHeroe(idx: number): Heroe {
+    return this.heroes[idx];
+  }
 }
 
 export interface Heroe {
